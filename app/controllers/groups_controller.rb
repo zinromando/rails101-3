9ecd76334c078e01021ccbaa3c3085b3799aeebd
zin <romandozin@gmail.com>
2017-01-01 22:46:49 +0100

    use before_action find_group_and_check_permission

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,6 @@
 class GroupsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
 
   def index
     @groups = Group.all
@@ -13,13 +14,7 @@ class GroupsController < ApplicationController
     @group = Group.find(params[:id])
   end
 
-  def edit
-    @group = Group.find(params[:id])
-
-    if current_user != @group.user
-      redirect_to root_path, alert: 'You have no permission.'
-    end
-  end
+  def edit; end
 
   def create
     @group = Group.new(group_params)
@@ -32,12 +27,6 @@ class GroupsController < ApplicationController
   end
 
   def update
-    @group = Group.find(params[:id])
-
-    if current_user != @group.user
-      redirect_to root_path, alert: 'You have no permission.'
-    end
-
     if @group.update(group_params)
       redirect_to groups_path, notice: 'Update Sucess'
     else
@@ -46,12 +35,6 @@ class GroupsController < ApplicationController
   end
 
   def destroy
-    @group = Group.find(params[:id])
-
-    if current_user != @group.user
-      redirect_to root_path, alert: 'You have no permission.'
-    end
-
     @group.destroy
     redirect_to groups_path, alert: 'Group deleted'
   end
@@ -60,5 +43,13 @@ class GroupsController < ApplicationController
 
   def group_params
     params.require(:group).permit(:title, :description)
+  end
+
+  def find_group_and_check_permission
+    @group = Group.find(params[:id])
+
+    if current_user != @group.user
+      redirect_to root_path, alert: 'You have no permission.'
+    end
   end
 end
